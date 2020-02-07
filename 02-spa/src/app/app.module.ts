75bbbe15a5f85983206e211cb572c08de42e0f4a
Liseth20 <40703021+Liseth20@users.noreply.github.com>
2020-02-07 17:53:35 -0600

    Programa 2

--- 02-spa/src/app/app.module.ts
+++ 02-spa/src/app/app.module.ts
@@ -6,7 +6,7 @@ import { APP_ROUTING } from './app.routes';
 
 
 //Servicios
-
+import {HeroesService} from './services/heroes.service';
 
 //Componentes
 import { AppComponent } from './app.component';
@@ -14,6 +14,9 @@ import { NavbarComponent } from './components/shared/navbar/navbar.component';
 import { AboutComponent } from './components/about/about.component';
 import { HeroesComponent } from './components/heroes/heroes.component';
 import { HomeComponent } from './components/home/home.component';
+import { HeroeComponent } from './components/heroe/heroe.component';
+import { BuscadorComponent } from './components/buscador/buscador.component';
+import { HeroeTarjetaComponent } from './components/heroe-tarjeta/heroe-tarjeta.component';
 
 @NgModule({
   declarations: [
@@ -21,13 +24,18 @@ import { HomeComponent } from './components/home/home.component';
     NavbarComponent,
     AboutComponent,
     HeroesComponent,
-    HomeComponent
+    HomeComponent,
+    HeroeComponent,
+    BuscadorComponent,
+    HeroeTarjetaComponent
   ],
   imports: [
     BrowserModule,
     APP_ROUTING
   ],
-  providers: [],
+  providers: [
+    HeroesService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
