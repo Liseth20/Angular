27a89323dd96e4d1c51a0595659f21460fc2d20c
Liseth20 <40703021+Liseth20@users.noreply.github.com>
2020-02-15 15:52:14 -0600

    fin del programa de Auth0

--- 07-authapp/src/app/components/protegida/protegida.component.ts
+++ 07-authapp/src/app/components/protegida/protegida.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../../services/auth.service';
 
 @Component({
   selector: 'app-protegida',
@@ -7,9 +8,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProtegidaComponent implements OnInit {
 
-  constructor() { }
+  constructor( public auth: AuthService) { }
 
   ngOnInit() {
+ 
   }
 
 }
