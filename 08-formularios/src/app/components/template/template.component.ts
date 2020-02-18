dba32ccd0d2e4acd86ea28e5ef3c352194124eb8
Liseth20 <40703021+Liseth20@users.noreply.github.com>
2020-02-17 21:02:57 -0600

    funciones desde el html

---
+++ 08-formularios/src/app/components/template/template.component.ts
@@ -0,0 +1,40 @@
+import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+
+@Component({
+  selector: 'app-template',
+  templateUrl: './template.component.html',
+  styles: [`
+  .ng-invalid.ng-touched:not(form){
+    border:1px solid red;
+  }
+  `]
+})
+export class TemplateComponent implements OnInit {
+
+usuario = {
+  nombre:null,
+  apellido: null,
+  email: null,
+  pais: ""
+}
+paises= [{
+codigo: "CRI",
+nombre: "Costa Rica"
+},
+{
+  codigo: "Esp",
+ nombre: "España"
+}]
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  guardar(forma: NgForm){
+
+    console.log( 'ngForm:', forma);
+    console.log( 'Valor forma', forma.value);
+    console.log( 'Usuario', this.usuario);
+  }
+}
