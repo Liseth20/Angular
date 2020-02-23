448ab25fd780163960fe7f7c0687bc12d189bf52
Liseth20 <40703021+Liseth20@users.noreply.github.com>
2020-02-23 13:19:04 -0600

    trabajando en el chat

--- 10-firechat/src/app/app.module.ts
+++ 10-firechat/src/app/app.module.ts
@@ -3,16 +3,31 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FormsModule } from '@angular/forms';
+
+
+import { environment } from '../environments/environment';
+
+import { AngularFireModule } from '@angular/fire';
+import { AngularFireAnalyticsModule } from '@angular/fire/analytics';
+import { AngularFirestoreModule } from '@angular/fire/firestore';
+import { ChatComponent } from './components/chat/chat.component';
+import { ChatService } from './providers/chat.service';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ChatComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFireAnalyticsModule,
+    AngularFirestoreModule,
+    FormsModule
   ],
-  providers: [],
+  providers: [ChatService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- 10-firechat/src/environments/environment.ts
+++ 10-firechat/src/environments/environment.ts
@@ -1,16 +1,15 @@
-// This file can be replaced during build by using the `fileReplacements` array.
-// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
-// The list of file replacements can be found in `angular.json`.
+
 
 export const environment = {
-  production: false
+  production: false, 
+  firebase: {
+    apiKey: "AIzaSyAq5x33vbkbyXuAfIgyD55PTiZqdkZf7nQ",
+    authDomain: "firechat-9ef9f.firebaseapp.com",
+    databaseURL: "https://firechat-9ef9f.firebaseio.com",
+    projectId: "firechat-9ef9f",
+    storageBucket: "firechat-9ef9f.appspot.com",
+    messagingSenderId: "693122630830",
+    appId: "1:693122630830:web:ca3254eaf78b811315593f",
+    measurementId: "G-3KTQF8EB47"
+  }
 };
-
-/*
- * For easier debugging in development mode, you can import the following file
- * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
- *
- * This import should be commented out in production mode because it will have a negative impact
- * on performance if an error is thrown.
- */
-// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
